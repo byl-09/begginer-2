7410824cc5fd06a571d328f29499aed14a25e1ce
byl-09 <9800_1@mail.ru>
2023-03-02 11:00:23 +0300

    массивы

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello');
+
+
+
+const url = 'https://catalog.onliner.by/notebook/hp/27k19ea';
+
+
+function fff(url){
+const res = url.split('/');
+console.log(res);
+const [protocol, _, adress, ...others] = res;
+console.log(`протокол - ${protocol} \nадресс - ${adress} \nurl - /${others.join('/')} `);
+}
+
+fff(url);
